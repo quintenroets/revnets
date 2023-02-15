7f51c7babbe1ff0efeeac7c63452bb832b9fc82a
Quinten Roets <quinten.roets@gmail.com>
2023-02-15 08:02:43 +0000

    show name of technique

--- revnets/main/experiment.py
+++ revnets/main/experiment.py
@@ -1,8 +1,22 @@
 from dataclasses import dataclass
 
+import cli
+
 from .. import evaluations, networks, reconstructions
 from ..networks.base import Network
-from ..utils import config, rank_zero_only
+from ..utils import Path, config, rank_zero_only
+
+
+def get_technique_name(technique):
+    techniques_path = Path(reconstructions.__file__).parent
+    technique_path = Path(technique.__file__)
+    sub_name = technique_path.relative_to(techniques_path).with_suffix("")
+    return str(sub_name).capitalize()
+
+
+def show_name(technique):
+    name = get_technique_name(technique)
+    cli.console.print(f"[underline]{name}[/underline]")
 
 
 @dataclass
@@ -14,8 +28,9 @@ class Experiment:
         original = network.get_trained_network()
 
         for technique in reconstructions.get_algorithms():
+            show_name(technique)
             reconstruction = network.get_architecture()
-            technique.reconstruct(original, reconstruction)
+            technique.reconstruct(original, reconstruction, network)
             evaluations.evaluate(original, reconstruction)
 
     @classmethod
