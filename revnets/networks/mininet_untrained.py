7a75d13284c25433799fe0454be1396062686dfb
Quinten Roets <quinten.roets@gmail.com>
2023-02-15 07:34:58 +0000

    a

---
+++ revnets/networks/mininet_untrained.py
@@ -0,0 +1,24 @@
+import torch.random
+
+from ..data import mnist1d
+from . import train
+from .models import mininet
+
+
+class Network(train.Network):
+    @classmethod
+    def get_trained_network(cls):
+        seed = 27
+        torch.manual_seed(seed)
+        model = mininet.Model()
+        return model
+
+    @classmethod
+    def get_architecture(cls):
+        seed = 97
+        torch.manual_seed(seed)
+        return mininet.Model()
+
+    @classmethod
+    def dataset(cls):
+        return mnist1d.Dataset()
