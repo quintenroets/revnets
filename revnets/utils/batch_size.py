fix automatic batch size calculation

revnets/utils/batch_size.py
@@ -7,21 +7,20 @@ import torch.nn
 from cacher import decorator
 from cacher.caches import base
 
+from .config import config
+
 if typing.TYPE_CHECKING:
-    from MTL_labels.multitask.model import MultitaskModel  # noqa: autoimport
+    from revnets.networks.models.trainable import Model  # noqa: autoimport
 
 
 class TuneModel(pl.LightningModule):
-    def __init__(self, model):
+    def __init__(self, model, data):
         super().__init__()
-        self.model: MultitaskModel = model
+        self.model: Model = model
+        self.data = data
         self.batch_size = 1
-        self.old_batch_size = self.model.multitask.data.batch_size
+        self.old_batch_size = self.data.batch_size
         self.automatic_optimization = False
-
-    @property
-    def data(self):
-        return self.model.multitask.data
 
     def val_dataloader(self):
         self.data.eval_batch_size = self.batch_size
@@ -35,11 +34,7 @@ class TuneModel(pl.LightningModule):
         return self.data.train_dataloader()
 
     def training_step(self, batch, batch_idx):
-        # check if accumulation fits in memory as well
-        accumulate_interval = 2 if self.batch_size < self.old_batch_size else 1
-        self.model.multitask.combiner.accumulate_grad_batches = accumulate_interval
-        for _ in range(accumulate_interval):
-            return self.model.training_step(batch, batch_idx)
+        return self.model.training_step(batch, batch_idx)
 
     def configure_optimizers(self):
         return self.model.configure_optimizers()
@@ -51,14 +46,7 @@ class Reducer(base.Reducer):
         """We do not want a new cache entry for every weight assignment Cache
         calculated result for each model dataset and task names.
         """
-        config = model.multitask.config
-        return (
-            config.model,
-            config.dataset,
-            config.label_names,
-            config.combiner,
-            config.devices,
-        )
+        return config.network, config.devices
 
     @classmethod
     def reduce_datamodule(cls, _: pl.LightningDataModule):
@@ -70,8 +58,8 @@ cache = decorator.cache(Reducer)
 
 
 @cache
-def get_max_batch_size(model: MultitaskModel, method="validate"):
-    tune_model = TuneModel(model)
+def get_max_batch_size(model: Model, data, method="validate"):
+    tune_model = TuneModel(model, data)
     trainer = pl.Trainer(
         accelerator="auto",
         auto_scale_batch_size=True,
@@ -79,11 +67,11 @@ def get_max_batch_size(model: MultitaskModel, method="validate"):
         max_epochs=1,
         logger=False,
     )
-    scale_batch_size_kwargs = {"init_val": model.config.batch_size}
+    scale_batch_size_kwargs = {"init_val": config.batch_size}
     print(f"Calculating max {method} batch size")
 
     model.do_log = False
-    old_batch_size = model.multitask.data.batch_size
+    old_batch_size = data.batch_size
     try:
         trainer.tune(
             tune_model,
@@ -95,11 +83,9 @@ def get_max_batch_size(model: MultitaskModel, method="validate"):
         raise Exception(message)
 
     model.do_log = True
-    model.multitask.data.batch_size = old_batch_size
+    data.batch_size = old_batch_size
 
-    safety_factor = (
-        (4 if model.config.devices > 1 else 2) if method == "validate" else 1
-    )
+    safety_factor = (4 if config.devices > 1 else 2) if method == "validate" else 1
     max_batch_size = tune_model.batch_size // safety_factor
     max_batch_size |= 1  # max sure batch size at least one
     return max_batch_size