a8a0ebb88c639b48c54f8d8b6a5d186bed868098
Quinten Roets <quinten.roets@gmail.com>
2023-02-27 18:04:39 +0000

    migrate setup to setup.cfg

--- setup.py
+++ setup.py
@@ -1,30 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-NAME = "revnets"
-version = "0.0.1"
-
-
-def read(filename):
-    try:
-        with open(filename) as fp:
-            content = fp.read().split("\n")
-    except FileNotFoundError:
-        content = []
-    return content
-
-
-setup(
-    author="Judah Goldfeder & Quinten Roets",
-    author_email="qdr2104@columbia.edu",
-    description="",
-    name=NAME,
-    version=version,
-    packages=find_packages(),
-    setup_requires=read("setup_requirements.txt"),
-    install_requires=read("requirements.txt"),
-    entry_points={
-        "console_scripts": [
-            "revnets = revnets:main",
-        ]
-    },
-)
+if __name__ == "__main__":
+    setup()
