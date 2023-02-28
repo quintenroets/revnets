ac533ad9e26c1731c712a4e0c22c11a1df127211
Quinten <quinten.roets@gmail.com>
2023-02-28 04:01:52 -0500

    initialize deterministically

--- revnets/reconstructions/retrain_on_outputs_with_bigger_net.py
+++ revnets/reconstructions/retrain_on_outputs_with_bigger_net.py
@@ -1,7 +1,11 @@
+import torch
+
 from ..networks.models import mininet
 from . import retrain_on_outputs
 
 
 class Reconstructor(retrain_on_outputs.Reconstructor):
     def initialize_reconstruction(self):
+        seed = 97
+        torch.manual_seed(seed)
         return mininet.Model(hidden_size=256)
